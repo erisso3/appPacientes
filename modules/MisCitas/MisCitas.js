40cacafa878f8a30bdca03bb51bb92a9b97c8638
Victor <victoe680@gmail.com>
2021-05-15 11:17:15 -0500

    registro cita y listar citas

---
+++ modules/MisCitas/MisCitas.js
@@ -0,0 +1,84 @@
+import { Container, Header, Content, Button, Text, Card, CardItem, Footer, FooterTab, Icon, Item } from 'native-base';
+import React, { Component } from 'react';
+import { StyleSheet, FlatList } from 'react-native';
+
+import API from '../Utils/API'
+import ItemCita from './ItemCita';
+
+export default class MisCitas extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            listCitas: []
+        };
+    }
+
+    registrarCita = () => {
+        this.props.navigation.navigate('RegistroCita')
+    }
+
+    misCitas = () => {
+
+        this.props.navigation.navigate('MisCitas')
+    }
+
+    perfil = () => {
+        this.props.navigation.navigate('Perfil');
+    }
+
+    async componentDidMount() {
+        let respnose = await API.getCitas(1);
+        this.setState({ listCitas: respnose.lista });
+        //console.log(respnose);
+    }
+
+
+    render() {
+        let { listCitas } = this.state;
+
+        return (
+            <Container>
+                <Header>
+                    <Text style={styles.header}>Lista</Text>
+                </Header>
+                <Content>
+                    <FlatList
+                        data={listCitas}
+                        keyExtractor={(x, i) => i.toString()}
+                        renderItem={({ item }) => <ItemCita datos={item} navigation={this.props.navigation} />}
+                    >
+
+                    </FlatList>
+                </Content>
+                <Footer>
+                <FooterTab>
+                        <Button active vertical onPress={this.registrarCita}>
+                            <Icon name="newspaper" />
+                            <Text>Registro</Text>
+                        </Button>
+                        <Button vertical onPress={this.misCitas} >
+                            <Icon name="clipboard" />
+                            <Text>Citas</Text>
+                        </Button>
+                        <Button vertical onPress={this.perfil}>
+                            <Icon name="person" />
+                            <Text>User</Text>
+                        </Button>
+                    </FooterTab>
+                </Footer>
+            </Container>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    btnenviar: {
+        marginTop: '5%'
+    },
+    header: {
+        color: '#fff',
+        textAlignVertical: 'center',
+        fontSize: 25
+    }
+});
